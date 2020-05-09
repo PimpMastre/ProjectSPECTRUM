b006b909fd6d2da581abad099540527ea63de5f2
PimpMastre <pmpmstrr@hotmail.com>
2020-05-09 18:27:58 +0100

    Modified timing test with nice results

--- TESTING/timing_test.py
+++ TESTING/timing_test.py
@@ -2,6 +2,7 @@
 import time
 import RPi.GPIO as GPIO
 import random
+from timeit import default_timer
 
 # Import the WS2801 module.
 import Adafruit_WS2801
@@ -15,7 +16,7 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
 GPIO.setmode(GPIO.BCM)
-
+previous_timestamp = default_timer()
 
 def sensorCallback(channel):
     global previous_timestamp, pixels
@@ -23,14 +24,21 @@ def sensorCallback(channel):
         new_stamp = default_timer()
         stamp = new_stamp - previous_timestamp
         previous_timestamp = new_stamp
-        print(str(round(60/stamp)) + 'RPM: ' + str(stamp))
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
-        for i in range(pixels.count()):
-            pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(r, b, g))
-        pixels.show()
-        pixels.clear()
+	n = 10
+	for i in range(n):
+	    show_stamp = default_timer()
+	    for i in range(pixels.count()):
+                pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(r, b, g))
+            pixels.show()
+            pixels.clear()
+	    pixels.show()
+	    timeTaken = default_timer() - show_stamp
+	    slep = (stamp - timeTaken) / (n * 2)
+	   # print(slep)
+            time.sleep(slep)
 
 
 if __name__ == "__main__":
@@ -40,6 +48,13 @@ if __name__ == "__main__":
 
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(17, GPIO.BOTH, callback=sensorCallback)
+
+    try:
+        while True:
+	    time.sleep(0.1)
+    except KeyboardInterrupt:
+        GPIO.cleanup() 
+
 
 
 
