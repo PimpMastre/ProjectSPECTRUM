b858bd62d6ef4f2f592e4f4fb23601581e936451
PimpMastre <pmpmstrr@hotmail.com>
2020-05-25 00:38:33 +0300

    Refactor fixes

--- Master/audioProcessor.py
+++ Master/audioProcessor.py
@@ -1,12 +1,14 @@
-from Master.Utils.timerThread import TimerThread
+import numpy as np
+import pyaudio
+from Utils.timerThread import TimerThread
 
 
 class AudioProcessor:
-    def __init__(self, nb=5, lfm=20, hfm=22000, ac=1000000, ms=0, da=0, ppbl=2, ch=2, rate=44100, chunksz=2048, udp=None):
+    def __init__(self, nb=5, lcm=20, hcm=1024, ac=1000000, ms=0, da=200, ppbl=2, ch=2, rate=44100, chunksz=2048, udp=None):
         """
         :param nb: number of bars to interpret data with
-        :param lfm: lower range frequency cut, cuts the lower ranges by fcl percent
-        :param hfm: higher range frequency cut, cuts the higher ranges by fch percent
+        :param lcm: lower range chunk cut, cuts the lower ranges by fcl percent
+        :param hcm: higher range chunk cut, cuts the higher ranges by fch percent
         :param ac: amplitude clip, clips the amplitude values
         :param ms: mapping style, 0 = geomspace, 1 = linspace
         :param da: data amplification, amplifies results by da percent
@@ -27,8 +29,8 @@ class AudioProcessor:
 
         # data transformation parameters
         self.num_bars = nb
-        self.lower_frequency_margin = lfm
-        self.higher_frequency_margin = hfm
+        self.lower_chunk_margin = lcm
+        self.higher_chunk_margin = hcm
         self.mapping_style = ms
         self.amplitude_clip = ac
         self.data_amplification = da
@@ -63,26 +65,21 @@ class AudioProcessor:
         # TODO: ADD EQUALIZER
         plot_points = []
         if self.mapping_style == 0:
-            plot_points = np.geomspace(self.lower_frequency_margin, self.higher_frequency_margin, num=self.num_bars-1)
+            plot_points = np.geomspace(self.lower_chunk_margin, self.higher_chunk_margin, num=self.num_bars + 1)
         else:
-            plot_points = np.linspace(self.lower_frequency_margin, self.higher_frequency_margin, num=self.num_bars-1)
+            plot_points = np.linspace(self.lower_chunk_margin, self.higher_chunk_margin, num=self.num_bars + 1)
 
         absolute_data = np.clip(np.abs(self.spectrum_data), a_min=0, a_max=self.amplitude_clip)
         current_averages = []
-
+        
         for x in range(self.num_bars):
-            if x == 0:
-                section = absolute_data[:int(plot_points[0])]
-            elif x == self.num_bars - 1:
-                section = absolute_data[int(plot_points[-1]):]
-            else:
-                section = absolute_data[int(plot_points[x - 1]):int(plot_points[x])]
+            section = absolute_data[int(plot_points[x]):int(plot_points[x + 1])]
 
             # get average of current section
             section_average = np.average(section)
             amplification_value = self.data_amplification * section_average / 100
             current_averages.append(section_average + amplification_value)
-
+            
         self.update_prev_peaks(np.array(current_averages))
         self.processed_data = np.average(self.prev_peaks, axis=0)
 
